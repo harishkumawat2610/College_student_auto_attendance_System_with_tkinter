c9e1b3ca579f09f2ec96fac3aa059efe9cefebdf
HARISH KUMAWAT <39532868+harishkumawat2610@users.noreply.github.com>
2019-03-02 14:39:53 +0530

    Create all frames in gui

--- auto_attendance.py
+++ auto_attendance.py
@@ -1,9 +1,29 @@
-#import all classes from tkinter
+#import tkintter
 from tkinter import *
 
+#Create window
 root = Tk()
-root.title("Attendance System")
+
+#window title
+root.title("Ad-hoc Networks Attendance System")
+
+#Create topframe with top side
+topframe=Frame(root)
+topframe.pack(side=TOP)
+
+#Create bottomframe with bottom side
+bottomframe=Frame(root)
+
+#Create leftframe in bottomframe with  left side
+leftframe=Frame(bottomframe,bg='black')
+leftframe.pack(side=LEFT)
+
+#Create rightframe in bottomframe with right side
+rightframe=Frame(bottomframe,padx=50)
+rightframe.pack(side=RIGHT)
+#Set window geometry width 1210 and height 750 and open position on screen left to 150 and top to 150
 root.geometry("1210x750+150+150")
-#infinite loop
+#stable main window on infinity time
 root.mainloop()
+
 
