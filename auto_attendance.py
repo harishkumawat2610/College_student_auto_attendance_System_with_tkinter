3b21aa64a2560e4703d2dd5adece20da8c8c2d72
HARISH KUMAWAT <39532868+harishkumawat2610@users.noreply.github.com>
2019-03-06 18:40:46 +0530

    upload all button

--- auto_attendance.py
+++ auto_attendance.py
@@ -37,6 +37,50 @@ canvas.create_image(50,10,image=photo,anchor=NW)
 leftframe.pack(side=LEFT)
 #Create rightframe in bottomframe with right side
 rightframe=Frame(bottomframe,padx=50)
+#create all image path for buttons and checkbox images
+photo1=PhotoImage(file='//home//kuma-company//Attedance_tkinter//add.png')
+photo2=PhotoImage(file='//home//kuma-company//Attedance_tkinter//start.png')
+photo3=PhotoImage(file='//home//kuma-company//Attedance_tkinter//stop.png')
+photo4=PhotoImage(file='//home//kuma-company//Attedance_tkinter//file.png')
+photo5=PhotoImage(file='//home//kuma-company//Attedance_tkinter//exit.png')
+photo6=PhotoImage(file='//home//kuma-company//Attedance_tkinter//clock.png')
+photo7=PhotoImage(file='//home//kuma-company//Attedance_tkinter//camera1.png')
+photo8=PhotoImage(file='//home//kuma-company//Attedance_tkinter//upimg.png')
+
+#button bt1 for add new entry
+Bt1=Button(rightframe,text="New Entry",image=photo1,activebackground="green", bd=0,width=10)
+Bt1.pack(fill=X)
+
+#button bt2 for start webcam
+Bt2=Button(rightframe,text="Start",image=photo2,activebackground="green", bd=0)
+Bt2.pack(fill=X,pady=10)
+
+##button bt3 for stop camera
+Bt3=Button(rightframe,text="Stop(Press'Q')",image=photo3,activebackground="green", bd=0)
+Bt3.pack(fill=X,pady=10)
+
+##button bt4 for seen files
+Bt4=Button(rightframe,text="files",width=10,image=photo4,activebackground="green", bd=0)
+Bt4.pack(fill=X,pady=10)
+
+##button bt5 for Exit programme
+Bt5=Button(rightframe,text="EXIT",image=photo5,activebackground="green", bd=0)
+Bt5.pack(fill=X,pady=10)
+
+#check_bt for set time period in minutes
+h=StringVar()
+check_bt=Checkbutton(rightframe,text="Set Time",variable=h, offvalue="uncheck",onvalue="check",activeforeground="green",width=120,image=photo6,compound=TOP)
+check_bt.pack(side=LEFT)
+
+#check_bt2 for upload images if person(student notpresent ) or offline making data 
+M=StringVar()
+check_bt2=Checkbutton(rightframe,text="Upload Image",variable=M, offvalue="uncheck",onvalue="check",activeforeground="green",width=120,image=photo8,compound=TOP)
+
+#check_bt3 for set camera or change camera
+k=StringVar()
+check_bt3=Checkbutton(rightframe,text="Set Camera",variable=k, offvalue="cameraoff",onvalue="cameraon",activeforeground="green",width=130,image=photo7,compound=TOP)
+check_bt3.pack(side=RIGHT)
+
 rightframe.pack(side=RIGHT)
 bottomframe.pack()
 #Set window geometry width 1210 and height 750 and open position on screen left to 150 and top to 150
